2bfb96be7fdbec5639bead7a025f6126bf1bcca8
FarwaNadeem <66826986+FarwaNadeem@users.noreply.github.com>
2024-09-23 10:58:45 +0500

    custom header code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { Editor, EditorState, convertToRaw } from "draft-js";
+import "draft-js/dist/Draft.css"; // Import default Draft.js styles
+import React, { useState } from "react";
+import { Document, Packer, Paragraph, Header } from "docx";
+import "./App.css"; // our app styles
+import { saveAs } from "file-saver";
 
 function App() {
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const headerText = "boooooom our header text";
+
+  const downloadDoc = () => {
+    const contentState = editorState.getCurrentContent(); // content state of editor
+    const rawContentState = convertToRaw(contentState); // raw content of editor
+
+    // paragraphs from the editor content
+    const paragraphs = rawContentState.blocks.map(
+      (block) => new Paragraph(block.text)
+    );
+
+    // code from docx library to have the header and editor text
+    const doc = new Document({
+      sections: [
+        {
+          headers: {
+            default: new Header({
+              children: [
+                new Paragraph({
+                  text: headerText,
+                  alignment: "center",
+                }),
+              ],
+            }),
+          },
+          children: [...paragraphs],
+        },
+      ],
+    });
+
+    // generate and download the DOCX file
+    Packer.toBlob(doc).then((blob) => {
+      saveAs(blob, "document.docx");
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="document-editor">
+      <button className="btn" onClick={downloadDoc}>
+        Download DOCX
+      </button>
+      <div className="editor-area">
+        <div className="page-header">
+          <p>{headerText}</p>
+        </div>
+        <Editor editorState={editorState} onChange={setEditorState} />
+      </div>
     </div>
   );
 }
